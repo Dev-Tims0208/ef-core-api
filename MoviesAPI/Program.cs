0d67e026f8525e420b278563723ece836b5c86c8
Dev-Tims0208 <eroy.timothy@gmail.com>
2022-09-15 22:34:07 +0800

    - transform error into arrays

--- MoviesAPI/Program.cs
+++ MoviesAPI/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using MoviesAPI;
+using MoviesAPI.APIBehavior;
 using MoviesAPI.Filters;
 
 var policyName = "_myAllowSpecificOrigins";
@@ -11,7 +12,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers(options =>
 {
     options.Filters.Add(typeof(MyExceptionFilter));
-});
+    options.Filters.Add(typeof(ParseBadRequest));
+}).ConfigureApiBehaviorOptions(BadRequestsBehavior.Parse);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
